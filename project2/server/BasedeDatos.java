3f707487a70866353ff6a2ba1c6209812bd9bb4b
frankzapata <43896392+frankzapata@users.noreply.github.com>
2019-01-03 12:59:30 -0400

    Update BasedeDatos.java

--- project2/server/BasedeDatos.java
+++ project2/server/BasedeDatos.java
@@ -1,10 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package proyecto;
-
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
