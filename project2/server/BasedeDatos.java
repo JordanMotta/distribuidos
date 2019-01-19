3a2150b9f301f1bcb826081519ee9b8edf870c96
frankzapata <43896392+frankzapata@users.noreply.github.com>
2019-01-19 16:37:06 -0400

    comentarios

--- project2/server/BasedeDatos.java
+++ project2/server/BasedeDatos.java
@@ -13,6 +13,11 @@ public class BasedeDatos {
        archivo = nombre del archivo 
        texto = es la informacion que tiene ese archivo
     */
+        /*
+    metodo que sirve para crear archivos este tiene un usuario maestro que es responsable de crear por primera vez el archivo 
+    el nombre es la persona que esta actualizando o creando el archivo , el archivo es el nombre del archivo y el texto es el
+    contenido de este.
+    */
     public String Creararchivo(String Maestro , String nombre, String archivo , String texto){
         
         String respuesta = "hola";
@@ -21,14 +26,22 @@ public class BasedeDatos {
         Date date = new Date();
         DateFormat hourdateFormat = new SimpleDateFormat("HH:mm:ss dd/MM/yyyy");
         String hora = hourdateFormat.format(date);
+        // verifica si hay un maestro con el mismo nombre si existe entonces este toma el hash que se le dio si no existe 
+        // entonces este se le asigna un hash 
         String hash = xml.Verificar("Maestro", Maestro);
+        // si es null entonces significa que no existe el maestro en el archivo proyecto.xml
         if ("null".equals(hash)){
+            // funcion que crear el archivo y las carpeta con sus hash respectivamente y guarda el archivo con su contenido
             funcion(hora,archivo,xml,Maestro,archivos,nombre,texto);
+            // si este exite entonces se busca si existe el archivo en el archivo proyecto.xml con el maestro 
         }else {
             String tem = xml.Verificar("archivo", archivo);
             if ("null".equals(tem) ){
+                // si no existe el archivo se procede a crear la carpeta con su hash y guardar el archivo con su contenido
                 funcion(hora,archivo,xml,Maestro,archivos,nombre,texto);
             }else {
+                // si este existe entonces se porcede a buscar la carpeta que contiene el archivo que se va modificar 
+                // se actualiza el archivo hash.xml (actualizacion) y se guarda el archivo con su contenido 
                 String ruta =url+"\\"+tem;
                 XmlHash xmlhash = new XmlHash(ruta);
                 hash= xmlhash.Crear(hourdateFormat.format(date), nombre);
@@ -38,6 +51,9 @@ public class BasedeDatos {
         }
         return respuesta;
     }
+     //  esta funcion es el proceso de almacenar el archivo con su contenido este crea  o actualiza el proyecto.xml 
+    // luego crea el fichero con el hash para luego este cree el archivo con su contenido y lo guarde en el fichero
+    // ya  antes mencionado 
     private void funcion (String hora , String archivo, Xml xml , String Maestro , archivos archivos , String nombre ,String texto){
                 String hash= xml.Crear( archivo, hora,Maestro);
                 archivos.CrearFichero(url+"\\"+hash);
@@ -47,7 +63,7 @@ public class BasedeDatos {
                 archivos.CrearArchivo(ruta+"\\"+archivo+"_"+hash , texto);
             }
     /*
-      
+      es una mostrar el nombre del archivo con el maestro y  las personas que modificaron dicho archivo 
     */
     public void Mostrar_por_nombre (String Maestro, String nombre ){
          Xml xml =new Xml();
